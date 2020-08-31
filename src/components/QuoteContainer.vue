683af55031191afd8fde0b9fa1778616eb2e13d5
yujialves <yujialves@i.softbank.jp>
2020-09-01 00:12:56 +0900

    Add QuoteContainer component

---
+++ src/components/QuoteContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="row">
+      <quote v-for="(quote, idx) in quotes" :key="idx" @click.native="deleteQuote(idx)">{{ quote }}</quote>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  props: {
+    quotes: Array,
+  },
+  components: {
+    Quote,
+  },
+  methods: {
+    deleteQuote(idx) {
+      this.$emit("deleteQuote", idx);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
